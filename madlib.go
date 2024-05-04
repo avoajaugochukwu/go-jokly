b8451f5de32c3395f4f15a1e309426a6848805e2
Ugochukwu Avoaja <avoaja@Ugochukwus-MacBook-Pro.local>
2024-05-04 19:55:37 -0400

    Moved madlib randInt into function

--- madlib.go
+++ madlib.go
@@ -5,8 +5,6 @@ import (
 	"math/rand"
 )
 
-var randInt = rand.Intn(7)
-
 var names = []string{"John", "Joy", "Rebecca", "Rob", "Smith", "Roger", "Hayley"}
 var occupations = []string{"doctor", "teacher", "engineer", "lawyer", "scientist", "chef", "artist"}
 var computingDevices = []string{"laptop", "smartphone", "tablet", "desktop", "smartwatch", "smartglasses", "smartshoes"}
@@ -15,5 +13,6 @@ var moodAdjectives = []string{"happy", "sad", "angry", "excited", "bored", "tire
 var deviceActions = []string{"play", "pause", "stop", "rewind", "fast forward", "record", "delete"}
 
 func GetRandomMadlib() string {
+	var randInt = rand.Intn(7)
 	return fmt.Sprintf("%s is a a %d year old %s who has been struggling with a lot of job-related stress. He/she decided to try a new application to relieve stress, which runs on a/an %s to help improve his/her mood.\n\nThe application senses his/her mood through a device he/she wears on his/her %s.\n\nWhen the device senses that he/she is %s, it respond by %s.", names[randInt], randInt+18, occupations[randInt], computingDevices[randInt], bodyParts[randInt], moodAdjectives[randInt], deviceActions[randInt])
 }
--- readme.md
+++ readme.md
@@ -1,12 +1,19 @@
 # Jokly Go App
 A simple go app that showcases the use of APIs and reusable functions.
 
-### How to run
+### How to run without Docker
 
 - Clone repo
 - Go to directory that has the code
 - Open terminal
 - Run `go build -o jokly`
 - Then run `./jokly`
+- Visit `http://localhost:8080` on your browser
 
+
+### How to run with Docker
+- Clone repo
+- Go to directory that has the code
+- Open terminal
+- Run `docker-compose up --build`
 #### Thanks
